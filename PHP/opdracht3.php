f473cd2a098f1bfaa9900a8e65858f9bf15fb90e
Abdel5314 <113344711+Abdel5314@users.noreply.github.com>
2023-02-09 14:37:29 +0100

    Add files via upload

---
+++ PHP/opdracht3.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php 
+$x = "Abdelghani Al Abdalaoui";
+$y = 'Klas=1G';
+
+echo $x;
+echo "<br>"; 
+echo $y;
+echo "<br>";
+$naam = "Abdelghani Al Abdalaoui !";
+
+echo "$naam";
+echo "<br>";
+$denk = "numberssss";
+
+echo $denk;
+echo "<br>";
+?>
+
+</body>
+</html>
